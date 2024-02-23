9ec72c83c926b5cdb395830f84adcdc801354395
Jagdish Das <115186984+jagdishdas@users.noreply.github.com>
2024-02-24 00:02:51 +0500

    Add files via upload

---
+++ picture.py
@@ -0,0 +1,64 @@
+### Picture Visual Appearance Analyzer
+from dotenv import load_dotenv
+
+load_dotenv() ## load all the environment variables
+
+import streamlit as st
+import os
+import google.generativeai as genai
+from PIL import Image
+
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+## Function to load Google Gemini Pro Vision API And get response
+
+def get_gemini_repsonse(input,image,prompt):
+    model=genai.GenerativeModel('gemini-pro-vision')
+    response=model.generate_content([input,image[0],prompt])
+    return response.text
+
+def input_image_setup(uploaded_file):
+    # Check if a file has been uploaded
+    if uploaded_file is not None:
+        # Read the file into bytes
+        bytes_data = uploaded_file.getvalue()
+
+        image_parts = [
+            {
+                "mime_type": uploaded_file.type,  # Get the mime type of the uploaded file
+                "data": bytes_data
+            }
+        ]
+        return image_parts
+    else:
+        raise FileNotFoundError("No file uploaded")
+    
+##initialize our streamlit app
+
+st.set_page_config(page_title="Picture Visual Appearance Analyzer")
+
+st.header("Picture Visual Appearance Analyzer")
+input=st.text_input("Input Prompt: ",key="input")
+uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
+image=""   
+if uploaded_file is not None:
+    image = Image.open(uploaded_file)
+    st.image(image, caption="Uploaded Image.", use_column_width=True)
+
+
+submit=st.button("Photo Expert Go ahead")
+
+input_prompt="""
+Evaluate the visual appeal of the given image on a scale of 1 to 10, considering composition, color palette, contrast, focus on main subject or object, what does the picture wants to show or convey through visual data and balance. Identify any missing key features. Provide specific enhancement guidelines, including recommendations for optimal lighting, alternative angles, and suggestions for additional elements, to elevate the overall visual appeal.
+and also show camera settings to keep in dslr or mirrorless camera.
+
+"""
+
+## If submit button is clicked
+
+if submit:
+    image_data=input_image_setup(uploaded_file)
+    response=get_gemini_repsonse(input_prompt,image_data,input)
+    st.subheader("The Response is")
+    st.write(response)
+
